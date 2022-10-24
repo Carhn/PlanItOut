0b5031fb1ab7d74e1d77762e772daf1b0e44e559
Logan Samstag <lms737@nau.edu>
2022-10-24 10:51:12 -0700

    Made reminders read in string

--- planner_cs386/lib/pages/reminders.dart
+++ planner_cs386/lib/pages/reminders.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:planner_cs386/Resources/colors.dart';
 import 'package:planner_cs386/Resources/routes.dart';
 
+class Reminder {
+  String name = '';
+  int time = 0;
+}
+
 // ignore: use_key_in_widget_constructors
 class Reminders extends StatefulWidget {
   @override
@@ -9,6 +14,9 @@ class Reminders extends StatefulWidget {
 }
 
 class _ReminderState extends State<Reminders> {
+  TextEditingController controller = TextEditingController();
+  String reminderName = "";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,13 +29,21 @@ class _ReminderState extends State<Reminders> {
               children: const <Widget>[Text('PlantItOut'), Text('Reminders')])),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Reminders Goes here!',
-            ),
-            Text(
-              'More to come soon',
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: const TextStyle(fontSize: 20),
+                foregroundColor: Colors.blueGrey,
+              ),
+              onPressed: () async {
+                final reminderName = await openDialog(context, controller);
+
+                //              if (reminderName == null || reminderName.isEmpty)
+
+                print('Name is $reminderName');
+              },
+              child: const Text('Add Reminder'),
             ),
           ],
         ),
@@ -75,3 +91,25 @@ class _ReminderState extends State<Reminders> {
     ));
   }
 }
+
+Future<String?> openDialog(
+        BuildContext context, TextEditingController controller) =>
+    showDialog<String?>(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Add Reminder Name'),
+        content: TextField(
+          autofocus: true,
+          decoration: InputDecoration(hintText: 'Enter Reminder Name'),
+          controller: controller,
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: Text('SUBMIT'),
+            onPressed: () {
+              Navigator.of(context).pop(controller.text);
+            },
+          ),
+        ],
+      ),
+    );
