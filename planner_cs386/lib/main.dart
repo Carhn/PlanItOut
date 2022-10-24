c4e79acb565d67d2407af6d300dc60d75541017a
paytonious <paytonhcox@gmail.com>
2022-10-24 11:13:00 -0700

    Calendar set up

--- planner_cs386/lib/main.dart
+++ planner_cs386/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:planner_cs386/Resources/colors.dart';
 import 'package:planner_cs386/Resources/routes.dart';
 import 'package:planner_cs386/pages/reminders.dart';
 import 'package:planner_cs386/pages/todoList.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -19,20 +20,15 @@ void main() {
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  CalendarFormat format = CalendarFormat.month;
+  DateTime _selectedDay = DateTime.now();
+  DateTime _focusedDay = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,19 +36,39 @@ class _HomeState extends State<Home> {
           backgroundColor: planItOutPrimary,
           title: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const <Widget>[Text('PlantItOut'), Text('Calendar')])),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Calendar Goes here!',
-            ),
-            Text(
-              'More to come soon',
-            ),
-          ],
-        ),
+              children: const <Widget>[
+                Text(
+                  'PlantItOut',
+                  style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+                ),
+                Text('Calendar')
+              ])),
+      body: TableCalendar(
+        focusedDay: _selectedDay,
+        firstDay: DateTime(1990),
+        lastDay: DateTime(2050),
+        calendarFormat: format,
+        onFormatChanged: (CalendarFormat _format) {
+          setState(() {
+            format = _format;
+          });
+        },
+        startingDayOfWeek: StartingDayOfWeek.sunday,
+        calendarStyle: const CalendarStyle(
+            isTodayHighlighted: true,
+            selectedDecoration: BoxDecoration(
+              color: planItOutPrimary,
+              shape: BoxShape.circle,
+            )),
+        selectedDayPredicate: (day) {
+          return isSameDay(_selectedDay, day);
+        },
+        onDaySelected: (selectedDay, focusedDay) {
+          setState(() {
+            _selectedDay = selectedDay;
+            _focusedDay = focusedDay; // update `_focusedDay` here as well
+          });
+        },
       ),
       bottomNavigationBar: BottomAppBar(
         color: planItOutPrimary,
