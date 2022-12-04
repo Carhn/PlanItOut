26eccb28e8ee9ee4edfa43ce3db2572e687f49c1
Erick Salazar <e.salazar.mexica1@gmail.com>
2022-12-03 21:55:42 -0800

    added new test for todo list

--- planner_cs386/test/todo_test.dart
+++ planner_cs386/test/todo_test.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:planner_cs386/main.dart';
 import 'package:planner_cs386/pages/todoList.dart';
 
-
 void main() {
-  testWidgets('Counter increment for button press', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget( Todo() );
-
-    expect(find.widgetWithIcon(FloatingActionButton, Icons.add), findsOneWidget);
+  // test to check item is added when tapping button
+  test('New task added', () {
+    Todo testVal = Todo(name: "test", checked: true);
+    String result = testVal.name;
+    expect(result, "test");
   });
 }
