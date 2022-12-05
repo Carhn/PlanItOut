ecc9f39e474d956795e3437787dbc603611aed82
Erick Salazar <e.salazar.mexica1@gmail.com>
2022-12-04 19:20:42 -0800

    added test for dialog box

--- planner_cs386/test/todo_test.dart
+++ planner_cs386/test/todo_test.dart
@@ -4,16 +4,23 @@ import 'package:planner_cs386/main.dart';
 import 'package:planner_cs386/pages/todoList.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
   // test to check item is added when tapping button
   test('New task added', () {
     Todo testVal = Todo(name: "test", checked: true);
     String result = testVal.name;
     expect(result, "test");
   });
-  
-   // test to check that an additional dialog box is opened when tapping the '+' button
-  test('Dialog Box was opened and identified successfully', () {
-    expect(
-        find.widgetWithIcon(FloatingActionButton, Icons.add), findsOneWidget);
+
+  // test to find add button in dialog box
+  testWidgets('finds add button', (tester) async {
+    // Define the test key.
+    const testKey = Key('Add');
+
+    // Build a MaterialApp with the testKey.
+    await tester.pumpWidget(MaterialApp(key: testKey, home: Container()));
+
+    // Find the MaterialApp widget using the testKey.
+    expect(find.byKey(testKey), findsOneWidget);
   });
 }
